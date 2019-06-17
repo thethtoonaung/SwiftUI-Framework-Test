d531b63235e802f53ad8aa8cfb44b661a4febd1d
Han Htoo Naung <thethtuenaung@gmail.com>
2019-06-17 14:23:59 +0630

    Initial Commit

---
+++ SwiftUI Framework Test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI Framework Test
+//
+//  Created by HaN  on 6/17/19.
+//  Copyright © 2019 HaN . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
